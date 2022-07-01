8915a015a459dbe84045729807014c98e53b2178
Eazzycodes <ilorioisrael@gmail.com>
2022-07-01 05:47:50 -0700

    If-else-while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,18 +3,18 @@
 #include <time.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 		char low;
 
-			for (low = 'a'; low <= 'z'; low++)
-						putchar(low);
+		for (low = 'a'; low <= 'z'; low++)
+					putchar(low);
 					putchar('\n');
 
-						return (0);
+		return (0);
 }

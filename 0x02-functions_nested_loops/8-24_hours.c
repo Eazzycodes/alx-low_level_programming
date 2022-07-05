e28d0731e7f6fb6c0d420733044d09a4025596e9
Eazzycodes <ilorioisrael@gmail.com>
2022-07-05 13:20:29 -0700

    functions nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,24 @@
 #include "main.h"
 
 /**
- * main - check code
- * Return: Always 0.
+ * jack_bauer - Check Code
+ * Description: function prints every minute in one day
+ * Return: Nothing
  */
-void jack_bauer(void);
+void jack_bauer(void)
 {
-	jack_bauer();
-	return (0);
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }

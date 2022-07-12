fdef6a327f85f27084c1f42c24e3fdc58b427eef
Eazzycodes <ilorioisrael@gmail.com>
2022-07-12 11:06:10 -0700

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -27,17 +27,17 @@ int _strlen(char *s)
 void rev_string(char *s)
 {
 
-	  int i, len;
-	  char a, z;
+	 int i, len;
+	 char a, z;
 
-	  len = _strlen(s) - 1;
-	  i = 0;
-
-	  while (i < len)
-	  {
+	 len = _strlen(s) - 1;
+	 i = 0;
+	 while (i < len)
+	 {
 		  a = s[i];
 		  z = s[len];
 		  s[i++] = z;
 		  s[len--] = a;
-	  }
+	 }
+
 }

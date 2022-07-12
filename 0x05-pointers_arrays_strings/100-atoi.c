d54b1e8727a95f807cc73815af28dca0c062431a
Eazzycodes <ilorioisrael@gmail.com>
2022-07-12 12:29:51 -0700

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * _atoi - a function that converts string to integer
- *
- *   * @s: An input string
- *
- *    * Return: integer from conversion
- *
- *     */
+ * _atoi - a function that converts string to integer
+ * @s: An input string
+ * Return: integer from conversion
+ */
 
 int _atoi(char *s)
 
@@ -21,6 +15,7 @@ int _atoi(char *s)
 
 	while (*s)
 	{
+
 		if (*s == '-')
 			sign *= -1;
 

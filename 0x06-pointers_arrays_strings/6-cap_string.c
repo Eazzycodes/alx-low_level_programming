688c2e1e8757a91396ec1517a1650d0bf0b542fa
Eazzycodes <ilorioisrael@gmail.com>
2022-07-14 16:27:40 -0700

    pointers arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,10 @@
 int check_seperators(char c);
 
 /**
- *  * cap_string - a function that capitalizes all words of a string.
- *   * @s: An input string to capitalize letters
- *    * Return: pointer to s
- *     */
-
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to capitalize letters
+ * Return: pointer to s
+ */
 char *cap_string(char *s)
 {
 	int i = 0;
@@ -29,7 +28,6 @@ char *cap_string(char *s)
  * @c: an input character
  * Return: 1 if seperator, 0 otherwise
  */
-
 int check_seperators(char c)
 {
 	int i = 0;
